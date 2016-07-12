3aea0b939934965efaaace3a51d42c62764e0451
Vadim Sikora <vadim.sikora@divio.ch>
2016-07-12 14:27:43 +0200

    disable justifying

--- djangocms_text_ckeditor/static/djangocms_text_ckeditor/ckeditor_plugins/cmswidget/plugin.js
+++ djangocms_text_ckeditor/static/djangocms_text_ckeditor/ckeditor_plugins/cmswidget/plugin.js
@@ -13,6 +13,11 @@
         },
 
         init: function (editor) {
+            this.addWidgetDefinition(editor);
+            this.setupJustifyCommandHandlers(editor);
+        },
+
+        addWidgetDefinition: function (editor) {
             editor.widgets.add('cmswidget', (function widgetDef() {
                 return {
                     button: 'CMS Plugin',
@@ -21,7 +26,8 @@
                         '<cms-plugin style="unset: all">' +
                         '</cms-plugin>',
 
-                    allowedContent: 'cms-plugin[*]',
+                    allowedContent: 'cms-plugin',
+                    disallowedContent: 'cms-plugin{float}',
 
                     requiredContent: 'cms-plugin',
 
@@ -35,9 +41,41 @@
                         if (contents.css('display') !== 'inline') {
                             this.wrapper.addClass('cke_widget_wrapper_force_block');
                         }
+
+                        this.on('select', function () {
+                            editor.getCommand('justifyleft').disable();
+                            editor.getCommand('justifyright').disable();
+                            editor.getCommand('justifycenter').disable();
+                        });
+
+                        this.on('deselect', function () {
+                            editor.getCommand('justifyleft').enable();
+                            editor.getCommand('justifyright').enable();
+                            editor.getCommand('justifycenter').enable();
+                        });
                     }
                 };
             })());
+        },
+
+        setupJustifyCommandHandlers: function (editor) {
+            var commands = ['left', 'center', 'right'];
+
+            commands.forEach(function (command) {
+                var justifyCommand = editor.getCommand('justify' + command);
+
+                if (!justifyCommand) {
+                    return;
+                }
+
+                var originalRefresh = justifyCommand.refresh.bind(justifyCommand);
+
+                justifyCommand.refresh = function (ckeditor, path) {
+                    if (this.state !== CKEDITOR.TRISTATE_DISABLED) {
+                        originalRefresh(ckeditor, path);
+                    }
+                };
+            });
         }
     });
 })(CMS.$);
